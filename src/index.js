b9c96ed663e9c14fa1276b893f8d6ecd1b147e46
Antia Navajas <antianavajasgarrido@gmail.com>
2024-03-21 20:18:32 +0100

    creating endpoint GET by id and POST

--- src/index.js
+++ src/index.js
@@ -4,14 +4,10 @@ const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql2/promise');
 
-// create and config server
-//server.use(express.json({ limit: '25mb' }));
-
 const server = express();
 server.use(cors());
-server.use(express.json());
+server.use(express.json({limit:"25mb"}));
 
-// init express aplication
 const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
@@ -20,42 +16,79 @@ server.listen(serverPort, () => {
 // CONFIGURACIÓN DE MYSQL
 async function getConnection() {
   const connection = await mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    database: process.env.MYSQL_DB,
-    user: process.env.MYSQL_USER,
+    host: process.env.MYSQL_HOST ||'127.0.0.1',
+    database: process.env.MYSQL_DB || 'recetas_db',
+    user: process.env.MYSQL_USER || 'root',
     password: process.env.MYSQL_PASS,
   });
-  await connection.connect();
-
-  console.log(
-    `Conexión establecida con la base de datos (identificador=${connection.threadId})`
-  );
+  connection.connect();
 
   return connection;
+
 }
+
+console.log(
+    `Conexión establecida con la base de datos (identificador=${serverPort})`
+  );
+
 
 //endpoints
 
 server.get('/api/recetas', async (req, res) => {
-  // Connectar con la base de datos
+
   const conn = await getConnection();
 
-  // LANZAR SELECT
+
   const queryRecetas = `SELECT * FROM recetas`;
 
   const [results] = await conn.query(queryRecetas);
 
-  //Cerramos conexión
+
   conn.close();
 
   res.json({
-    info: { "count": numOfElements}, // número de elementos
-    results: results // listado
+    info: { "count": results.length},
+    results: results 
   });
 });
 
-//servidor estáticos
+server.get('/api/recetas/:id', async (req, res) => {
+    const { id } = req.params;
 
-const staticServerPathWeb = './src/public-react'; // En esta carpeta ponemos los ficheros estáticos
+    const conn = await getConnection();
+
+    const queryRecetaById = `SELECT * FROM recetas WHERE id = ?`;
+
+    const [results] = await conn.query(queryRecetaById, [id]);
+
+    conn.close();
+
+    res.json({
+        success: true,
+        receta: results[0] 
+      })
+
+});
+
+server.post('/api/recetas', async (req, res) => {
+    const {nombre, ingredientes, instrucciones } = req.body;
+
+    const conn = await getConnection();
+
+    const queryInsertReceta = `INSERT INTO recetas (nombre, ingredientes, instrucciones) VALUES (?, ?, ?)`;
+
+    const [result] = await conn.query(queryInsertReceta, [nombre, ingredientes, instrucciones]);
+
+    const nuevo_id = result.insertId;
+
+    conn.close();
+
+    res.json({
+        success: true,
+        id: nuevo_id
+      })
+
+});
+
+const staticServerPathWeb = './src/public-react'; 
 server.use(express.static(staticServerPathWeb));
-//importar bibliotecas
